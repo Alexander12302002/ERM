2ff2b8d8915fe344efc1dc713ba54ab504d0e580
Aki_1130 <106930173+Alexander12302002@users.noreply.github.com>
2024-06-23 11:51:49 -0500

    feat: :sparkles: finished queries of employee, offices, orders, payments, product and productlines

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise';
 export const connection = await mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net",
+    host: "roundhouse.proxy.rlwy.net",
     port: "33667",
     database: "railway",
     user:  "root",
